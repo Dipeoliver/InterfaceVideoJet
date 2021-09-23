9edf6598123d219e16c6d4d990a5986460b7e7c2
Diego Oliveira <dipeoliver@outlook.com>
2021-09-23 16:22:58 -0300

    first commit

---
+++ VideoJet/VideoJet/Form1.Designer.cs
@@ -0,0 +1,301 @@
+﻿namespace VideoJet
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+            this.lblSaida = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.lblImpressora = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.lblCliente = new System.Windows.Forms.Label();
+            this.serialPort2 = new System.IO.Ports.SerialPort(this.components);
+            this.serialPort3 = new System.IO.Ports.SerialPort(this.components);
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.rdbNaoImprimir = new System.Windows.Forms.RadioButton();
+            this.rdbImprimir = new System.Windows.Forms.RadioButton();
+            this.label1 = new System.Windows.Forms.Label();
+            this.rdbReprint = new System.Windows.Forms.RadioButton();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.lblCOM1 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.lblCOM4 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.toolStripStatusLabel2 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.lblCOM2 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.toolStripStatusLabel4 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.lblCOM3 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.serialPort4 = new System.IO.Ports.SerialPort(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // lblSaida
+            // 
+            this.lblSaida.AutoSize = true;
+            this.lblSaida.Font = new System.Drawing.Font("Arial", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblSaida.ForeColor = System.Drawing.Color.Red;
+            this.lblSaida.Location = new System.Drawing.Point(39, 205);
+            this.lblSaida.Name = "lblSaida";
+            this.lblSaida.Size = new System.Drawing.Size(0, 75);
+            this.lblSaida.TabIndex = 0;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Arial", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(38, 164);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(115, 32);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "SERIAL";
+            // 
+            // lblImpressora
+            // 
+            this.lblImpressora.AutoSize = true;
+            this.lblImpressora.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblImpressora.Location = new System.Drawing.Point(583, 50);
+            this.lblImpressora.Name = "lblImpressora";
+            this.lblImpressora.Size = new System.Drawing.Size(38, 31);
+            this.lblImpressora.TabIndex = 7;
+            this.lblImpressora.Text = "...";
+            this.lblImpressora.Visible = false;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(49, 109);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(53, 16);
+            this.label4.TabIndex = 0;
+            this.label4.Text = "Cliente";
+            // 
+            // lblCliente
+            // 
+            this.lblCliente.AutoSize = true;
+            this.lblCliente.Font = new System.Drawing.Font("Arial Narrow", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCliente.Location = new System.Drawing.Point(125, 100);
+            this.lblCliente.Name = "lblCliente";
+            this.lblCliente.Size = new System.Drawing.Size(0, 31);
+            this.lblCliente.TabIndex = 0;
+            // 
+            // serialPort2
+            // 
+            this.serialPort2.PortName = "COM2";
+            // 
+            // serialPort3
+            // 
+            this.serialPort3.PortName = "COM3";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.pictureBox1.Image = global::VideoJet.Properties.Resources.Preloader_1;
+            this.pictureBox1.Location = new System.Drawing.Point(610, 265);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(36, 28);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 12;
+            this.pictureBox1.TabStop = false;
+            // 
+            // rdbNaoImprimir
+            // 
+            this.rdbNaoImprimir.AutoSize = true;
+            this.rdbNaoImprimir.BackColor = System.Drawing.SystemColors.Control;
+            this.rdbNaoImprimir.Font = new System.Drawing.Font("Arial Narrow", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rdbNaoImprimir.Location = new System.Drawing.Point(271, 12);
+            this.rdbNaoImprimir.Name = "rdbNaoImprimir";
+            this.rdbNaoImprimir.Size = new System.Drawing.Size(188, 35);
+            this.rdbNaoImprimir.TabIndex = 2;
+            this.rdbNaoImprimir.Text = "NÃO IMPRIMIR";
+            this.rdbNaoImprimir.UseVisualStyleBackColor = false;
+            this.rdbNaoImprimir.CheckedChanged += new System.EventHandler(this.rdbNaoImprimir_CheckedChanged);
+            // 
+            // rdbImprimir
+            // 
+            this.rdbImprimir.AutoSize = true;
+            this.rdbImprimir.Checked = true;
+            this.rdbImprimir.Font = new System.Drawing.Font("Arial Narrow", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rdbImprimir.Location = new System.Drawing.Point(12, 12);
+            this.rdbImprimir.Name = "rdbImprimir";
+            this.rdbImprimir.Size = new System.Drawing.Size(223, 35);
+            this.rdbImprimir.TabIndex = 1;
+            this.rdbImprimir.TabStop = true;
+            this.rdbImprimir.Text = "IMPRIMIR + GMES";
+            this.rdbImprimir.UseVisualStyleBackColor = true;
+            this.rdbImprimir.CheckedChanged += new System.EventHandler(this.rdbImprimir_CheckedChanged);
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(599, 296);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(47, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Ver.1.05";
+            // 
+            // rdbReprint
+            // 
+            this.rdbReprint.AutoSize = true;
+            this.rdbReprint.BackColor = System.Drawing.SystemColors.Control;
+            this.rdbReprint.Font = new System.Drawing.Font("Arial Narrow", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rdbReprint.Location = new System.Drawing.Point(495, 12);
+            this.rdbReprint.Name = "rdbReprint";
+            this.rdbReprint.Size = new System.Drawing.Size(130, 35);
+            this.rdbReprint.TabIndex = 3;
+            this.rdbReprint.Text = "REPRINT";
+            this.rdbReprint.UseVisualStyleBackColor = false;
+            this.rdbReprint.CheckedChanged += new System.EventHandler(this.rdbReprint_CheckedChanged);
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.lblCOM1,
+            this.toolStripStatusLabel1,
+            this.lblCOM4,
+            this.toolStripStatusLabel2,
+            this.lblCOM2,
+            this.toolStripStatusLabel4,
+            this.lblCOM3});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 309);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(659, 22);
+            this.statusStrip1.TabIndex = 13;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // lblCOM1
+            // 
+            this.lblCOM1.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold);
+            this.lblCOM1.Name = "lblCOM1";
+            this.lblCOM1.Size = new System.Drawing.Size(95, 17);
+            this.lblCOM1.Text = "Scanner   COM1";
+            // 
+            // toolStripStatusLabel1
+            // 
+            this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            this.toolStripStatusLabel1.Size = new System.Drawing.Size(12, 17);
+            this.toolStripStatusLabel1.Text = "/";
+            // 
+            // lblCOM4
+            // 
+            this.lblCOM4.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold);
+            this.lblCOM4.Name = "lblCOM4";
+            this.lblCOM4.Size = new System.Drawing.Size(92, 17);
+            this.lblCOM4.Text = "Scanner  COM4";
+            // 
+            // toolStripStatusLabel2
+            // 
+            this.toolStripStatusLabel2.Name = "toolStripStatusLabel2";
+            this.toolStripStatusLabel2.Size = new System.Drawing.Size(24, 17);
+            this.toolStripStatusLabel2.Text = "  /  ";
+            // 
+            // lblCOM2
+            // 
+            this.lblCOM2.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold);
+            this.lblCOM2.Name = "lblCOM2";
+            this.lblCOM2.Size = new System.Drawing.Size(112, 17);
+            this.lblCOM2.Text = "Impressora   COM2";
+            // 
+            // toolStripStatusLabel4
+            // 
+            this.toolStripStatusLabel4.Name = "toolStripStatusLabel4";
+            this.toolStripStatusLabel4.Size = new System.Drawing.Size(24, 17);
+            this.toolStripStatusLabel4.Text = "  /  ";
+            // 
+            // lblCOM3
+            // 
+            this.lblCOM3.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold);
+            this.lblCOM3.Name = "lblCOM3";
+            this.lblCOM3.Size = new System.Drawing.Size(83, 17);
+            this.lblCOM3.Text = "GMES   COM3";
+            // 
+            // serialPort4
+            // 
+            this.serialPort4.PortName = "COM4";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(659, 331);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.rdbReprint);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.rdbImprimir);
+            this.Controls.Add(this.rdbNaoImprimir);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.lblCliente);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.lblImpressora);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.lblSaida);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Interface Scanner GMES & VideoJet";
+            this.TopMost = true;
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.IO.Ports.SerialPort serialPort1;
+        private System.Windows.Forms.Label lblSaida;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label lblImpressora;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label lblCliente;
+        private System.IO.Ports.SerialPort serialPort2;
+        private System.IO.Ports.SerialPort serialPort3;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.RadioButton rdbNaoImprimir;
+        private System.Windows.Forms.RadioButton rdbImprimir;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.RadioButton rdbReprint;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel lblCOM1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel2;
+        private System.Windows.Forms.ToolStripStatusLabel lblCOM2;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel4;
+        private System.Windows.Forms.ToolStripStatusLabel lblCOM3;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
+        private System.Windows.Forms.ToolStripStatusLabel lblCOM4;
+        private System.IO.Ports.SerialPort serialPort4;
+    }
+}
+
---
+++ VideoJet/VideoJet/Form1.cs
@@ -0,0 +1,311 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO.Ports;
+
+namespace VideoJet  // interface scanner e VideoJet
+{
+    public partial class Form1 : Form
+    {
+        string Entrada;
+        string Impressora;
+        private delegate void SetTextDelege(string data);
+
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            rdbImprimir.BackColor = Color.LimeGreen;
+            RS232();
+
+            try
+            {
+                // abre conecao com as portas e muda cor da label.
+                serialPort1.Open();
+                if (serialPort1.IsOpen)
+                {
+                    lblCOM1.ForeColor = Color.Green;
+                }
+
+                serialPort2.Open();
+                if (serialPort2.IsOpen)
+                {
+                    lblCOM2.ForeColor = Color.Green;
+                }
+
+                serialPort3.Open();
+                if (serialPort3.IsOpen)
+                {
+                    lblCOM3.ForeColor = Color.Green;
+                }
+
+
+                serialPort4.Open();
+                if (serialPort4.IsOpen)
+                {
+                    lblCOM4.ForeColor = Color.Green;
+                }
+            }
+            catch
+            {
+                // se nao conseguir conexao com a porta muda cor da label e exibe mensagem.
+                if (!serialPort1.IsOpen)
+                {
+                    lblCOM1.ForeColor = Color.Red;
+                }
+
+                else if (!serialPort2.IsOpen)
+                {
+                    lblCOM2.ForeColor = Color.Red;
+                }
+
+                else if (!serialPort3.IsOpen)
+                {
+                    lblCOM3.ForeColor = Color.Red;
+                }
+
+                else if (!serialPort4.IsOpen)
+                {
+                    lblCOM4.ForeColor = Color.Red;
+                }
+
+                MessageBox.Show("Porta Serial  ja em Uso", "ERRO", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                pictureBox1.Visible = false;
+                return;
+            }
+
+            serialPort1.DataReceived += new SerialDataReceivedEventHandler(DataReceivedHandler);
+            serialPort4.DataReceived += new SerialDataReceivedEventHandler(DataReceivedHandler);
+        }
+
+        private void RS232() // configura as portas seriais
+        {
+            serialPort1.PortName = "COM1";   //scanner
+            serialPort1.BaudRate = Convert.ToInt16(9600);
+            serialPort1.Parity = Parity.None;
+            serialPort1.StopBits = StopBits.One;
+            serialPort1.DataBits = 8;
+
+
+
+            serialPort4.PortName = "COM4";   //scanner
+            serialPort4.BaudRate = Convert.ToInt16(9600);
+            serialPort4.Parity = Parity.None;
+            serialPort4.StopBits = StopBits.One;
+            serialPort4.DataBits = 8;
+
+
+            serialPort2.PortName = "COM2";   //  impressora
+            serialPort2.BaudRate = Convert.ToInt32(9600);
+            serialPort2.Parity = Parity.None;
+            serialPort2.StopBits = StopBits.One;
+            serialPort2.DataBits = 8;
+
+            serialPort3.PortName = "COM3";    // gmes
+            serialPort3.BaudRate = Convert.ToInt16(9600);
+            serialPort3.Parity = Parity.None;
+            serialPort3.StopBits = StopBits.One;
+            serialPort3.DataBits = 8;
+
+
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            // fecha as seriais ao finalizar o programa
+            serialPort1.Close();
+            serialPort2.Close();
+            serialPort3.Close();
+            serialPort4.Close();
+        }
+
+        private void DataReceivedHandler(object sender, SerialDataReceivedEventArgs e) // recebe sinal da serial
+        {
+            SerialPort sp = (SerialPort)sender;
+            System.Threading.Thread.Sleep(250);
+            string indata = sp.ReadExisting();
+
+            this.BeginInvoke(new SetTextDelege(DisplayToUI), new object[] { indata });
+
+        }
+
+        private void DisplayToUI(string displayData)  // recebe dados da serial e envia para as seriais 
+        {
+
+            Entrada = "";
+
+            Entrada += displayData.Trim();
+
+            lblSaida.Text = Entrada;
+
+            if (Entrada == "CLEAR" || Entrada == "TRACE" || Entrada == "CBPASS")
+            {
+                serialPort3.WriteLine(Entrada + "\r\n");
+                lblCliente.Text = ".......";
+                rdbNaoImprimir.Checked = true;
+
+            }
+            else
+            {
+                //---------------------------------------------------------------------------------------------     LG  ---------------------------------------------------------
+                if (Entrada.Length == 12) // etiquetas LG tem  12 caracteres. 
+                {
+                    string V1;
+                    string V2;
+                    string V3;
+
+                    V1 = Entrada.Substring(0, 3);
+                    V2 = Entrada.Substring(3, 4);
+                    V3 = Entrada.Substring(7, 5);
+
+                    Impressora = "JDI|1|V1=" + V1 + "|V2=" + V2 + "|V3=" + V3 + "|" + "\r\n";
+
+                    // envia sinal para o GMES e para a impressora
+                    if (rdbImprimir.Checked == true)
+                    {
+
+                        lblImpressora.Text = Impressora;
+                        serialPort2.WriteLine(Impressora);
+                        serialPort3.WriteLine(Entrada + "\r\n");
+                        lblCliente.Text = "LG";
+                        // System.Threading.Thread.Sleep(250);
+                    }
+                    // envia sinal somente para o GMES 
+
+                    else if (rdbNaoImprimir.Checked == true)
+                    {
+                        serialPort3.WriteLine(Entrada + "\r\n");
+                        lblCliente.Text = "LG";
+
+                    }
+                    // envia sinal somente para o impressora
+                    else
+                    {
+                        serialPort2.WriteLine(Impressora);
+                        // System.Threading.Thread.Sleep(250);
+                        lblCliente.Text = "- REPRINT LG -";
+                    }
+                }
+                //-------------------------------------------------------------------------------------------     HP      ----------------------------------------------------------------
+
+                else if (Entrada.Length == 10) // etiquetas HP tem  10 caracteres.
+                {
+
+                    string V1;
+                    string V2;
+                    string V3;
+
+                    V1 = Entrada.Substring(0, 5);
+                    V2 = Entrada.Substring(5, 1);
+                    V3 = Entrada.Substring(6, 4);
+
+                    Impressora = "JDI|1|V1=" + V1 + "|V2=" + V2 + "|V3=" + V3 + "|" + "\r\n";
+
+                    // envia sinal para o GMES e para a impressora
+                    if (rdbImprimir.Checked == true)
+                    {
+
+                        lblImpressora.Text = Impressora;
+                        serialPort2.WriteLine(Impressora);
+                        serialPort3.WriteLine(Entrada + "\r\n");
+                        lblCliente.Text = "HP";
+                        // System.Threading.Thread.Sleep(250);
+                    }
+                    // envia sinal somente para o GMES 
+
+                    else if (rdbNaoImprimir.Checked == true)
+                    {
+                        serialPort3.WriteLine(Entrada + "\r\n");
+                        lblCliente.Text = "HP";
+                    }
+                    // envia sinal somente para o impressora
+                    else
+                    {
+                        serialPort2.WriteLine(Impressora);
+                        //System.Threading.Thread.Sleep(250);
+                        lblCliente.Text = "- REPRINT HP -";
+                    }
+
+                }
+                //---------------------------------------------------------------------------------    POSITIVO   /  DATEN    ----------------------------------------------------------------------
+
+                else if (Entrada.Length == 9) // etiquetas POSITIVO tem  9 caracteres.
+                {
+
+                    string V1;
+                    string V2;
+                    string V3;
+
+                    V1 = Entrada.Substring(0, 4);
+                    V2 = Entrada.Substring(4, 5);
+                    V3 = "#";
+
+                    Impressora = "JDI|1|V1=" + V1 + "|V2=" + V2 + "|V3=" + V3 + "|" + "\r\n";
+
+                    // envia sinal para o GMES e para a impressora
+                    if (rdbImprimir.Checked == true)
+                    {
+
+                        lblImpressora.Text = Impressora;
+                        serialPort2.WriteLine(Impressora);
+                        serialPort3.WriteLine(Entrada + "\r\n");
+                        lblCliente.Text = "POSITIVO";
+                        // System.Threading.Thread.Sleep(250);
+                    }
+                    // envia sinal somente para o GMES 
+
+                    else if (rdbNaoImprimir.Checked == true)
+                    {
+                        serialPort3.WriteLine(Entrada + "\r\n");
+                        lblCliente.Text = "POSITIVO";
+                    }
+                    // envia sinal somente para o impressora
+                    else
+                    {
+                        serialPort2.WriteLine(Impressora);
+                        //System.Threading.Thread.Sleep(250);
+                        lblCliente.Text = "- REPRINT POSITIVO -";
+                    }
+
+                }
+            }
+        }
+        
+        //-------    CONFIGURA AS CORES DOS CHECK BOX -------------
+        private void rdbImprimir_CheckedChanged(object sender, EventArgs e)
+        {
+            if (rdbImprimir.Checked == true)
+            {
+                rdbImprimir.BackColor = Color.LimeGreen;
+                rdbNaoImprimir.BackColor = DefaultBackColor;
+                rdbReprint.BackColor = DefaultBackColor;
+            }
+        }
+
+        private void rdbNaoImprimir_CheckedChanged(object sender, EventArgs e)
+        {
+            if (rdbNaoImprimir.Checked == true)
+            {
+                rdbImprimir.BackColor = DefaultBackColor;
+                rdbNaoImprimir.BackColor = Color.OrangeRed;
+                rdbReprint.BackColor = DefaultBackColor;
+            }
+        }
+
+        private void rdbReprint_CheckedChanged(object sender, EventArgs e)
+        {
+            rdbImprimir.BackColor = DefaultBackColor;
+            rdbNaoImprimir.BackColor = DefaultBackColor;
+            rdbReprint.BackColor = Color.Yellow;
+        }
+    }
+}
